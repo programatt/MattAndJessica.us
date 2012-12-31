86466df71ff319f935f21785aad9be4c79a40573
Matt Phillips <mathewrphillips@gmail.com>
2012-12-31 15:39:52 -0600

    added other pages

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,4 +11,40 @@ public class Application extends Controller {
     return ok(index.render("Hello World!"));
   }
   
+  public static Result about(){
+	return ok(about.render());
+  }
+  
+  public static Result proposal(){
+	return ok(proposal.render());
+  }
+  
+  public static Result ceremony(){
+	return ok(ceremony.render());
+  }
+  
+  public static Result wedding_party(){
+	return ok(weddingparty.render());
+  }
+  
+  public static Result registries(){
+	return ok(registries.render());
+  }
+  
+  public static Result guest_info(){
+	return ok(guestinfo.render());
+  }
+  
+  public static Result photo_album(){
+	return ok(photoalbum.render());
+  }
+  
+  public static Result guest_book(){
+	return ok(guestbook.render());
+  }
+  
+  public static Result honeymoon(){
+	return ok(honeymoon.render());
+  }
+  
 }
