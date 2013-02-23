91a60e4d701c0a65aa7c557d4cc82213f27d4fb5
Matt Phillips <mathewrphillips@gmail.com>
2013-02-23 06:22:14 -0600

    forgot to commit all of it

--- mandj/app.js
+++ mandj/app.js
@@ -31,7 +31,9 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/about', routes.about);
 app.get('/proposal', routes.proposal);
-
+app.get('/ceremony', routes.ceremony);
+app.get('/reception', routes.reception);
+app.get('/weddingparty', routes.weddingparty);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- mandj/routes/index.js
+++ mandj/routes/index.js
@@ -16,3 +16,14 @@ exports.about = function(req, res){
 exports.proposal = function(req, res){
     res.render('proposal', {title: 'The Proposal'});
 };
+
+exports.ceremony = function(req, res){
+    res.render('ceremony', {title: 'The Ceremony'}); }
+
+exports.reception = function(req, res){
+    res.render('reception', {title: 'The Reception'}); 
+}
+
+exports.weddingparty = function(req,res){
+    res.render('weddingparty', {title: 'The Wedding Party'});
+}
